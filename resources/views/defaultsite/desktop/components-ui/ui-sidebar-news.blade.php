391e37f6581306be5fbdbcde184b4b1c6f99ce52
megis <atanmegis.cn@gmail.com>
2022-09-19 10:56:52 +0700

    feat: sidebar news

---
+++ resources/views/defaultsite/desktop/components-ui/ui-sidebar-news.blade.php
@@ -0,0 +1,19 @@
+<div class="berita-sidebar-container">
+    <h4>berita sidebar</h4>
+    <a href="#">
+        <figure>
+            <img src="{{ $sb[1]['news_image']['real'] }}" width="100%" height="170px">
+            <figcaption>{{ $sb[1]['news_title'] }}</figcaption>
+        </figure>
+    </a>
+    <div class="list-berita-sidebar mt-2">
+        <a href="#" class="d-flex align-items-center justify-content-between gap-3">
+            <img src="{{ $sb[2]['news_image']['real'] }}" width="100%" height="170px">
+            <p>{{ $sb[2]['news_title'] }}</p>
+        </a>
+        <a href="#" class="d-flex align-items-center justify-content-between gap-3">
+            <img src="{{ $sb[3]['news_image']['real'] }}" width="100%" height="170px">
+            <p>{{ $sb[3]['news_title'] }}</p>
+        </a>
+    </div>
+</div>
