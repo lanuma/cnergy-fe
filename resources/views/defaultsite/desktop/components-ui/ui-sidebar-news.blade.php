bea529bde3f57df731459d99dc74cf0697d6bd11
fadhilahumairah <fadhilahumairah24@gmail.com>
2022-09-20 11:18:43 +0700

    bug: fix berita sidebar css image

--- resources/views/defaultsite/desktop/components-ui/ui-sidebar-news.blade.php
+++ resources/views/defaultsite/desktop/components-ui/ui-sidebar-news.blade.php
@@ -8,11 +8,15 @@
     </a>
     <div class="list-berita-sidebar mt-2">
         <a href="#" class="d-flex align-items-center justify-content-between gap-3">
-            <img src="{{ $sb[2]['news_image']['real'] }}" width="100%" height="170px">
+            <div class="image">
+                <img src="{{ $sb[2]['news_image']['real'] }}" width="100%" height="100%">
+            </div>
             <p>{{ $sb[2]['news_title'] }}</p>
         </a>
         <a href="#" class="d-flex align-items-center justify-content-between gap-3">
-            <img src="{{ $sb[3]['news_image']['real'] }}" width="100%" height="170px">
+            <div class="image">
+                <img src="{{ $sb[3]['news_image']['real'] }}" width="100%" height="100%">
+            </div>
             <p>{{ $sb[3]['news_title'] }}</p>
         </a>
     </div>
