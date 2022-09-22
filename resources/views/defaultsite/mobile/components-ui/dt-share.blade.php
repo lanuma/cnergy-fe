fdd81620532700fe27d8841c867f340cb774c8d9
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-22 12:07:00 +0700

    add component article

---
+++ resources/views/defaultsite/mobile/components-ui/dt-share.blade.php
@@ -0,0 +1,7 @@
+<div class="dt-share">
+    <div class="share flex flex-wrap items-center mb-4">
+        <div class="share-item share-item--fb"><a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(url()->current() .'?utm_source=Mobile&utm_medium=facebook&utm_campaign=Share_Bottom' )}}" target="_blank"><i class="icon icons--share icon--share-fb"></i></a></div>
+        <div class="share-item share-item--wa"><a href="https://wa.me/?text={{ urlencode(url()->current() .'?utm_source=Mobile&utm_medium=whatsapp&utm_campaign=Share_Bottom' )}}" target="_blank"><i class="icon icons--share icon--share-wa"></i></a></div>
+        <div class="share-item share-item--tweet"><a href="https://twitter.com/intent/tweet?u={{ urlencode(url()->current().'?utm_source=Mobile&utm_medium=twitter&utm_campaign=Share_Bottom' )}}" target="_blank"><i class="icon icons--share icon--share-tweet"></i></a></div>
+    </div>
+  </div>
