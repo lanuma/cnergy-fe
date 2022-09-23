bc4162e00b91d26f0381f1a83e530f4a79ecd701
yudakly <48110348+yudakly@users.noreply.github.com>
2022-09-23 12:18:40 +0700

    add domain english-mm.kl-youniverse.com

--- config/domain.php
+++ config/domain.php
@@ -2,7 +2,7 @@
 
 return [
     [
-        'domains'=> ['demo.hyperlocal.kl-youniverse.com', 'demo.local.newshub.id', 'cnergy-fe.test', 'localhost'],
+        'domains'=> ['demo.hyperlocal.kl-youniverse.com', 'demo.local.newshub.id', 'cnergy-fe.test', 'localhost', 'english-mm.kl-youniverse.com'],
         'token'=> env('NEWSHUB_TOKEN', ''),
         'namespace'=> 'DefaultSite'
     ]
