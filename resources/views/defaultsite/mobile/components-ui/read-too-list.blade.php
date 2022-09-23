c00de48fb0365202e938efabf9c0b816d9b2b551
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-23 15:11:17 +0700

    add component article

--- resources/views/defaultsite/mobile/components-ui/read-too-list.blade.php
+++ resources/views/defaultsite/mobile/components-ui/read-too-list.blade.php
@@ -1,8 +1,9 @@
 <div class="baca-juga-list-container">
   <h4>baca juga</h4>
+  @foreach ($news as $item)
   <ul>
-    <li><a href="#">Sering Kecelakaan di MotoGP, Marquez Malah Bangga dn Merasa mendapat Peelajaran</a></li>
-    <li><a href="#">Marquez Pemenang MotoGP Aragon, Rossi Mengesankan</a></li>
-    <li><a href="#">Rossi Ikut MotoGP Aragon Bukan Juara</a></li>
+    <li><a href="{{ Src::detail($item) }}" aria-label="{{ $item['news_title'] ?? null }}">{{ $item['news_title'] }}</a></li>
+    
   </ul>
+  @endforeach 
 </div>
--- resources/views/defaultsite/mobile/pages/readpage.blade.php
+++ resources/views/defaultsite/mobile/pages/readpage.blade.php
@@ -9,14 +9,16 @@
 @include('defaultsite.mobile.components-ui.main-news-article')
 
 {{-- read too list --}}
-@include('defaultsite.mobile.components-ui.read-too-list')
-
+@if ($popular = \Data::popular() ?? null)
+@include('defaultsite.mobile.components-ui.read-too-list' , ['news' => $popular])
+@endif
 {{-- related tag --}}
 @include('defaultsite.mobile.components-ui.related-tag')
 
 {{-- related artikel --}}
-@include('defaultsite.mobile.components-ui.related-article', ['ra' => $row['news_related']] )
-
+@if ($latest = \Data::popular() ?? null)
+@include('defaultsite.mobile.components-ui.related-article', ['news' => $latest, 'title' => 'Berita Terbaru']) 
+@endif
 {{-- trending tag --}}
 @include('defaultsite.mobile.components-ui.trending-tag')
 
