6504d04c20137953bd4c9a606f86ea3f66fd3ee6
fadhilahumairah <fadhilahumairah24@gmail.com>
2022-09-23 23:26:36 +0700

    feat: add 404, add slider and list popular news to index template

--- resources/views/defaultsite/mobile/pages/tag.blade.php
+++ resources/views/defaultsite/mobile/pages/tag.blade.php
@@ -3,4 +3,19 @@
 @section('content')
 @include('defaultsite.mobile.components-ui.main-content-index-m')
 
+{{-- slider trending tapi data belum ada --}}
+@if ($popular = \Data::popular() ?? null)
+    @include('defaultsite.mobile.components-ui.slider', ['hl' => $popular, 'title' => 'Berita Populer'])
+@endif
+
+{{--list populer news--}}
+@if ($popular = \Data::popular() ?? null)
+    @include('defaultsite.mobile.components-ui.populer-news', ['hl' => $popular])
+@endif
+
+{{-- slider latest news --}}
+@if ($latest = \Data::latest() ?? null)
+    @include('defaultsite.mobile.components-ui.slider', ['hl' => $latest, 'title' => 'Berita Terbaru'])
+@endif
+
 @endsection
