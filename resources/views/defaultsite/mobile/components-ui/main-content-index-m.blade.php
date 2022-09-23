c8b96a17b38073a1609bfeb709c3f2afa915e044
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-23 23:47:31 +0700

    add

--- resources/views/defaultsite/mobile/components-ui/main-content-index-m.blade.php
+++ resources/views/defaultsite/mobile/components-ui/main-content-index-m.blade.php
@@ -1,11 +1,14 @@
 <div class="more-info-container-index">
-    <h1 class="index-h1">Pilkada Bandung 2024</h1>
-    <p class="index-tag">Peristiwa</p>
-        <p class="index-p">
-            Selain memiliki arti, sebuah monumen pun dibuat untuk mengenang sebuah peristiwa yang terjadi di masa lalu agar orang di kehidupan mendatang tetap ingat akan kejadian masa itu.
+    <h1 class="index-h1"> {{ $tag['name'] ?? null }}</h1>
+    {{-- <p class="index-tag">{{$headline['news_category']}}</p> --}}
+    <div class="desc-index">
+            <p class="index-p">
+                {{ $headline['news_content'] ?? null }} <span class="index-span">Selengkapnya</span>
         </p>
-    <p class="index-tentang">Tentang: Ini merupakan gelaran pemilihan Kepala Daerah Bandung Tahun 2024</p>
+    <p class="index-tentang">Tentang: {{ $headline['news_synopsis'] ?? null }}</p>
     <p class="index-lokasi">Lokasi Pelaksanaan: Bandung</p>
-    <p class="index-date">Tanggal Pelaksanaan: 18-20 April 2024</p>
+    <p class="index-date">Tanggal Pelaksanaan: {{ Util::date($headline['news_entry'], 'ago') }}</p>
+    </div>
+    
 </div>
     
--- resources/views/defaultsite/mobile/pages/tag.blade.php
+++ resources/views/defaultsite/mobile/pages/tag.blade.php
@@ -1,6 +1,7 @@
 @extends('defaultsite.mobile.layouts.ui-main')
 
 @section('content')
+
 @include('defaultsite.mobile.components-ui.main-content-index-m')
 
 @endsection
