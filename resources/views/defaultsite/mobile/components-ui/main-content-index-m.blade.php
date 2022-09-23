4865ed2fa88351364495e7a19452461897a81951
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-23 22:57:02 +0700

    add index

---
+++ resources/views/defaultsite/mobile/components-ui/main-content-index-m.blade.php
@@ -0,0 +1,11 @@
+<div class="more-info-container-index">
+    <h1 class="index-h1">Pilkada Bandung 2024</h1>
+    <p class="index-tag">Peristiwa</p>
+        <p class="index-p">
+            Selain memiliki arti, sebuah monumen pun dibuat untuk mengenang sebuah peristiwa yang terjadi di masa lalu agar orang di kehidupan mendatang tetap ingat akan kejadian masa itu.
+        </p>
+    <p class="index-tentang">Tentang: Ini merupakan gelaran pemilihan Kepala Daerah Bandung Tahun 2024</p>
+    <p class="index-lokasi">Lokasi Pelaksanaan: Bandung</p>
+    <p class="index-date">Tanggal Pelaksanaan: 18-20 April 2024</p>
+</div>
+    
