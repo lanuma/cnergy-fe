372469b2ea7e76b358602dc333c90bf3f653d50f
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-24 21:18:41 +0700

    fix tag merdeka trending tag

--- resources/views/defaultsite/mobile/components-ui/main-content-index-m.blade.php
+++ resources/views/defaultsite/mobile/components-ui/main-content-index-m.blade.php
@@ -1,13 +1,15 @@
 <div class="more-info-container-index">
+    {{-- @dump($tag) --}}
+    {{-- <p>{{ $tag['content'] ?? null }}</p> --}}
     <h1 class="index-h1"> {{ $tag['name'] ?? null }}</h1>
     {{-- <p class="index-tag">{{$headline['news_category']}}</p> --}}
     <div class="desc-index">
             <p class="index-p">
-                {{ $headline['news_content'] ?? null }} <span class="index-span">Selengkapnya</span>
+                {{ $tag['description'] ?? null }} <span class="index-span">Selengkapnya</span>
         </p>
-    <p class="index-tentang">Tentang: {{ $headline['news_synopsis'] ?? null }}</p>
+    {{-- <p class="index-tentang">Tentang: {{ $headline['news_synopsis'] ?? null }}</p> --}}
     <p class="index-lokasi">Lokasi Pelaksanaan: Bandung</p>
-    <p class="index-date">Tanggal Pelaksanaan: {{ Util::date($headline['news_entry'], 'ago') }}</p>
+    <p class="index-date">Tanggal Pelaksanaan: {{ Util::date($tag['date_entry'], 'ago') }}</p>
     </div>
     
 </div>
