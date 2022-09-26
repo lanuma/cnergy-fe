ab9b48f12b04daf9028dcf5c82ea42d1c30badb1
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-26 08:28:12 +0700

    fix component foto index tag

--- resources/views/defaultsite/mobile/pages/tag.blade.php
+++ resources/views/defaultsite/mobile/pages/tag.blade.php
@@ -7,9 +7,9 @@
 
 {{-- Slider kumpulan foto --}}
 {{-- @dump($feed) --}}
-@if ($latest = \Data::latest() ?? null)
-    @include('defaultsite.mobile.components-ui.slider-kumpulan-foto', ['hl' => $latest, 'title' => 'Foto ' ])
-@endif
+{{-- @if ($latest = \Data::latest() ?? null) --}}
+    @include('defaultsite.mobile.components-ui.photo-collection', [ 'title' => 'Foto ' ])
+{{-- @endif --}}
 
 
 {{-- adds-on --}}
