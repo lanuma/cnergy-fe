b77d0f68f48742dedcefe76619a33ed025074e22
fadhilahumairah <fadhilahumairah24@gmail.com>
2022-09-28 21:00:20 +0700

    bug: chage search layout

--- resources/views/defaultsite/mobile/pages/search.blade.php
+++ resources/views/defaultsite/mobile/pages/search.blade.php
@@ -1,11 +1,11 @@
-@extends('defaultsite.mobile.layouts.main')
+@extends('defaultsite.mobile.layouts.ui-main')
 
 @section('content')
 
 <div id="v5-content" style="min-height: 80vh; overflow: hidden;">
-			
+
     <gcse:searchresults-only></gcse:searchresults-only>
-    
+
 </div>
 
 @endsection
