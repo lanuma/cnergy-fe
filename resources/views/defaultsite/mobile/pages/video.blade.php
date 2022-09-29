0b6cf0b56aa88e9a6b07939da2299323baed2748
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-09-29 07:18:47 +0700

    add page video

--- resources/views/defaultsite/mobile/pages/video.blade.php
+++ resources/views/defaultsite/mobile/pages/video.blade.php
@@ -1,6 +1,6 @@
-@extends('defaultsite.mobile.layouts.main')
+@extends('defaultsite.mobile.layouts.ui-main')
 
-@push('styles')
+{{-- @push('styles')
 <style>
     .channel-ad_ad-headline{
         margin:15px 0;
@@ -9,21 +9,61 @@
         justify-content: center;
     }
 </style>
-@endpush
+@endpush --}}
 
-@push('preload')
+{{-- @push('preload')
 @if( $headline[0]['news_id'] ?? null )
 <link rel="preload" as="image" href="{{ Src::imgNewsCdn($headline[0], '375x208', 'webp') }}" />
 @endif
-@endpush
+@endpush --}}
 
 @section('content')
-<div class="channel-ad channel-ad_ad-headline">
+<ul class="main-breadcrumb flex items-center flex-wrap list-none m-4 pb-3" >
+    <li class="main-breadcrumb-item"><a href="/">Home</a></li>
+    {{-- @foreach ($headline['news_category'] as $r) --}}
+        {{-- @if ($loop->iteration==1) --}}
+            <li class="main-breadcrumb-item "><a href="#" style="color: red">Video</a></li>
+        {{-- @elseif($loop->iteration==2)
+            <li class="main-breadcrumb-item {{$loop->count==2?'active':''}}"><a href="{{ url(( $row['news_category'][0]['url']??'' ).'/'.( $row['news_category'][1]['url']??'' )) }}">{{$r['name']??null}}</a></li>
+        @elseif($loop->iteration==3)
+            <li class="main-breadcrumb-item {{$loop->count==3?'active':''}}"><a href="{{ url(( $row['news_category'][0]['url']??'' ).'/'.( $row['news_category'][1]['url']??'' ).'/'.( $row['news_category'][2]['url']??'' )) }}">{{$r['name']??null}}</a></li> --}}
+        {{-- @endif --}}
+    {{-- @endforeach --}}
+</ul>
+{{-- <div class="channel-ad channel-ad_ad-headline">
     {!! Util::getAds('headline') !!}
-</div>
-<div class="main-body px-4">
+</div> --}}
+<div class="main-body ">
     <div class="main-article">
         @if( $headline[0]['news_id'] ?? null )
+        <div class="main-news-container photo-container">
+            <div class="photo-container">
+                <a href="{{ Src::detail($headline[0]) }}" aria-label="{{ $headline[0]['news_title'] ?? null }}">
+                    <div class="image-news" >
+                    @include('image', [
+                            'source' => $headline[0],
+                            'force' => '375x208',
+                            'size' => '375x208',
+                            $headline['news_title'] ?? null,
+                        ])
+                    </div>
+                </a>
+                <div class="item-desc-photo text-white" >
+                    <p class="photo-desc-date">{{ Util::date($headline[0]['news_date_publish'], 'ago') }} </p>
+                    <a href="{{ Src::detail($headline[0]) }}" aria-label="{{ $headline[0]['news_title'] ?? null }}"><p class="photo-desc-p">{{ $headline[0]['news_title'] }}</p></a>
+                    <div  class="item-desc-type  d-flex " style="color: white" >
+                        @if( $headline[0]['news_type'] == 'video' )
+                        <i class="ps-5 py-1 fa-brands fa-youtube"></i> <p class="video-list">Putar Video</p></i>
+                        @endif
+                        @if( $headline[0]['news_type'] == 'photonews' )
+                        <i class="fa-solid fa-camera bg-red"><span class="">Lihat foto</span></i>
+                        @endif
+                    </div>
+                </div>
+            </div>
+          </div>
+          @endif
+        {{-- @if( $headline[0]['news_id'] ?? null )
         <div class="section section--headline -mx-4">
             <div class="section--headline-top">
                 <figure class="item item--headline">
@@ -47,8 +87,21 @@
                 </figure>
             </div>
         </div>
-        @endif
+        @endif --}}
+        <div class="py-3">
+            @include('defaultsite.mobile.components-ui.ads-on')
+        </div>
+
         @if( $feed )
+            @include( 'defaultsite.mobile.components-ui.listvideo', ['rows'=> $feed,'page'=> 'homepage','data'=> 'headline'])
+        @endif
+        @if( $popular )
+            @include( 'defaultsite.mobile.components-ui.listvideo', ['rows'=> $popular,'page'=> 'homepage','data'=> 'popular'])
+        @endif
+        @if( $latest )
+            @include( 'defaultsite.mobile.components-ui.listvideo', ['rows'=> $latest,'page'=> 'homepage','data'=> 'latest'])
+        @endif
+        {{-- @if( $feed )
             @include( 'defaultsite.mobile.components.listvideo', ['rows'=> $feed,'page'=> 'homepage','data'=> 'headline'])
         @endif
         @if( $popular )
@@ -56,7 +109,7 @@
         @endif
         @if( $latest )
             @include( 'defaultsite.mobile.components.listvideo', ['rows'=> $latest,'page'=> 'homepage','data'=> 'latest'])
-        @endif
+        @endif --}}
         {{-- <div class="section section--infscroll">
             <div class="section--infscroll-next flex flex-col items-center justify-end">
                 <div class="section--infscroll-next-loading"><img class="lazyload" data-src="{{ Src::asset('img/kmk.gif') }}" width="60" height="60" alt="gif"></div>
