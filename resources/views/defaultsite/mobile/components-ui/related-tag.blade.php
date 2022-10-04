527da2d44c9ac16c4a312147dbd317a2ba3287d5
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-10-04 07:40:21 +0700

    fix

--- resources/views/defaultsite/mobile/components-ui/related-tag.blade.php
+++ resources/views/defaultsite/mobile/components-ui/related-tag.blade.php
@@ -1,7 +1,7 @@
 @if ($row['news_tag'] ?? null)
     <div class="related-tag-container">
         @foreach ($row['news_tag'] as $r)
-            <a class="btn btn-related mx-2 "  href="{{ Src::detailTag($r) }}">{{ $r['tag_name'] ?? null }}</a>
+            <a class="btn btn-related mx-2 my-2"  href="{{ Src::detailTag($r) }}">{{ $r['tag_name'] ?? null }}</a>
         @endforeach
       </div>
 @endif
