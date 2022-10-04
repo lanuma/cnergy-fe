91d9502ba011f81da275349a9bc56b7ea85870a0
fadhilahumairah <fadhilahumairah24@gmail.com>
2022-10-04 07:57:58 +0700

    bug: fix category list main news

--- resources/views/defaultsite/mobile/components-ui/list_text.blade.php
+++ resources/views/defaultsite/mobile/components-ui/list_text.blade.php
@@ -2,18 +2,20 @@
 <article id="{{$r['news_id']}}" class="articles articles--text">
     <div class="card-news">
         <a href="{{ Src::detail($r) }}" aria-label="{{ $r['news_title'] ?? 'untitled' }}">
-            @include('image', [
-                'source' => $r,
-                'size' => '85x85',
-                $r['news_title'] ?? null,
-            ])
-        <div class="description" >
-            <div class="banner">
-                <p class="text-category" href="{{ Src::category($r) }}">{{ $r['category_name'] ?? (last($r['news_category'])['name'] ?? '') }}</p>
-                <span>{{ Util::date($r['news_date_publish'], 'ago') }}</span>
+            <div class="image-news">
+                @include('image', [
+                    'source' => $r,
+                    'size' => '85x85',
+                    $r['news_title'] ?? null,
+                ])
             </div>
-            <h4 class="text-title" href="{{ Src::detail($r) }}">{{ $r['news_title'] ?? null }}</h4>
-        </div>
+            <div class="description" >
+                <div class="banner">
+                    <p class="text-category" href="{{ Src::category($r) }}">{{ $r['category_name'] ?? (last($r['news_category'])['name'] ?? '') }}</p>
+                    <span>{{ Util::date($r['news_date_publish'], 'ago') }}</span>
+                </div>
+                <h4 class="text-title" href="{{ Src::detail($r) }}">{{ $r['news_title'] ?? null }}</h4>
+            </div>
         </a>
     </div>
 </article>
--- resources/views/defaultsite/mobile/components-ui/list_video.blade.php
+++ resources/views/defaultsite/mobile/components-ui/list_video.blade.php
@@ -1,41 +1,22 @@
 <article id="{{$r['news_id']}}">
-<div class="highlight-article-container ">
-    <a href="{{ Src::detail($r) }}" aria-label="{{ $r['news_title']??'untitled' }}">
-        <figure>
-            <div class="image-news" style="border-radius: 8px">
-                @include('image', [
-                  'source' => $r,
-                  'size' => '375x208',
-                  $r['news_title'] ?? null,
-              ])
-            </div>
-            <div class="banner">
-                <p>{{ $r['category_name'] ?? (last($r['news_category'])['name'] ?? '') }}</p>
-                <span>{{ Util::date(date('Y-M-d H:i:s', strtotime($r['news_date_publish'])), 'short') }}
-                </span>
-            </div>
-            <a style="color: black" href="{{ Src::detail($r) }}" > <h2 class="item-desc-highlight">{{ $r['news_title']??null }}</h2></a>
-            
-        </figure>
-    </a>
-</div>
-
-</article>
-{{-- <article id="{{$r['news_id']}}" class="articles articles--video">
-    <figure class="item item--video">
-        <a href="{{ Src::detail($r) }}" class="item-img aspect-[16/9] rounded-lg" aria-label="{{ $r['news_title']??'untitled' }}">
-            @include('image', ['source'=>$r, 'size'=>'380x214', $r['news_title']??null])
-            <span class="item-img-info">
-                <!--<span>00:00</span>-->
-            </span>
+    <div class="highlight-article-container ">
+        <a href="{{ Src::detail($r) }}" aria-label="{{ $r['news_title']??'untitled' }}">
+            <figure>
+                <div class="image-news" style="border-radius: 8px">
+                    @include('image', [
+                    'source' => $r,
+                    'size' => '375x208',
+                    $r['news_title'] ?? null,
+                ])
+                </div>
+                <div class="banner">
+                    <p>{{ $r['category_name'] ?? (last($r['news_category'])['name'] ?? '') }}</p>
+                    <span>{{ Util::date(date('Y-M-d H:i:s', strtotime($r['news_date_publish'])), 'short') }}
+                    </span>
+                </div>
+                <a style="color: black" href="{{ Src::detail($r) }}" > <h2 class="item-desc-highlight">{{ $r['news_title']??null }}</h2></a>
+                
+            </figure>
         </a>
-        <figcaption class="item-desc pt-4">
-            <div class="item-desc-header text-10 mb-1">
-                <a href="{{ Src::category($r) }}" class="item-desc-tag mr-1">{{ $r['category_name'] ?? (last($r['news_category'])['name'] ?? '') }}</a> 
-                <span class="item-desc-time">{{ Util::date(date('Y-M-d H:i:s', strtotime($r['news_date_publish'])), 'short') }}</span>
-            </div>
-            <h2 class="item-desc-title text-16 font-bold mb-2"><a href="{{ Src::detail($r) }}">{{ $r['news_title']??null }}</a></h2>
-        </figcaption>
-    </figure>
-</article> --}}
-
+    </div>
+</article>
