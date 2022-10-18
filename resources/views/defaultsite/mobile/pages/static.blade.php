78e231e50e03e053e51aba4faa9d74d7f11d8eb1
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-10-18 12:59:38 +0700

    add line-height

--- resources/views/defaultsite/mobile/pages/static.blade.php
+++ resources/views/defaultsite/mobile/pages/static.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div id="v5-content" class=" my-4 mx-4" style="min-height: 80vh; overflow: hidden; ">
-    <h1 class="dt-title text-30 font-bold mb-4 " style="align-items: center">{{$row['title']??null}}</h1>
+    <h1 class="dt-title text-30 font-bold mb-4 " style="text-align: center">{{$row['title']??null}}</h1>
     <div style="text-align: justify; line-height: 2.5em; ">{!! htmlspecialchars_decode($row['content']??null)!!}</div>
 </div>
 @endsection
