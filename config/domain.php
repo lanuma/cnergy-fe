a9819d024beffeb55c868e29c0877df74289b2a1
Angga Lanuma <lanuma.angga@gmail.com>
2022-11-22 13:21:38 +0700

    add trstdly domain
    Signed-off-by: Angga Lanuma <lanuma.angga@gmail.com>

--- config/domain.php
+++ config/domain.php
@@ -5,5 +5,10 @@ return [
         'domains'=> ['demo.hyperlocal.kl-youniverse.com', 'demo.local.newshub.id', 'cnergy-fe.test', 'localhost', 'english-mm.kl-youniverse.com'],
         'token'=> env('NEWSHUB_TOKEN', ''),
         'namespace'=> 'DefaultSite'
+    ],
+    [
+        'domains' => ['www.trstd.ly', 'trstd.ly'],
+        'token' => env('NEWSHUB_TOKEN_TRSTDLY', ''),
+        'namespace' => 'DefaultSite'
     ]
 ];
